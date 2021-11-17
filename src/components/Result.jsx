1b299e7e46be6a59785e816c3e88b4af7f3870ac
Van Minh <vanminhquangtri@gmail.com>
2021-11-17 08:57:18 +0700

    set up redux toolkit

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const Result = () => {
-    const storeNumber = useSelector((state) => {
-        return state.Number
-    });
-    console.log('storeNumber :>> ', storeNumber);
-    return (
-        <div>
-            <h1>Result</h1>
-            <button>{storeNumber?.count || 0}</button>
-        </div>
-    );
+  const storeNumber = useSelector((state) => {
+    return state.Number;
+  });
+  return (
+    <div>
+      <h1>Result</h1>
+      <button>{storeNumber?.count || 0}</button>
+    </div>
+  );
 };
 
 export default Result;
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,32 @@
-import rootReducers from "./root-reducer";
-import { configureStore } from '@reduxjs/toolkit'
-import { persistStore, persistReducer } from 'redux-persist';
+import rootReducers from './root-reducer';
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
-    key: 'REDUX-TOOLKIT-DEMO',
-    storage,
+  key: 'REDUX-TOOLKIT-DEMO',
+  storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducers);
 
 export const store = configureStore({
-    reducer: persistedReducer,
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
